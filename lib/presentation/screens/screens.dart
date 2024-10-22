6637547db8d9497ccd44a7dc21b4dcb82ac69a49
Adrian Garcia <adrianisa123575@outlook.com>
2024-10-22 04:22:08 -0500

    Configuracion de rutas

---
+++ lib/presentation/screens/screens.dart
@@ -0,0 +1,5 @@
+export 'package:widgets_app_4/presentation/screens/buttons/buttons_screen.dart';
+export 'package:widgets_app_4/presentation/screens/cards/cards_screen.dart';
+export 'package:widgets_app_4/presentation/screens/home/home_screen.dart';
+
+
