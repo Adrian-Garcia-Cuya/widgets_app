012381838a2c34554c1f136d053642819c6ccfb4
Adrian Garcia <adrianisa123575@outlook.com>
2024-10-29 04:31:11 -0500

    Screens creadas

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,5 +1,10 @@
+
+export 'package:widgets_app_4/presentation/screens/animated/animated_screen.dart';
+export 'package:widgets_app_4/presentation/screens/app_tutorial/app_tutorial_screen.dart';
 export 'package:widgets_app_4/presentation/screens/buttons/buttons_screen.dart';
 export 'package:widgets_app_4/presentation/screens/cards/cards_screen.dart';
 export 'package:widgets_app_4/presentation/screens/home/home_screen.dart';
-
-
+export 'package:widgets_app_4/presentation/screens/inifinite_scroll/infinite_scroll_screen.dart';
+export 'package:widgets_app_4/presentation/screens/progress/progress_screen.dart';
+export 'package:widgets_app_4/presentation/screens/snackbar/snackbar_screen.dart';
+export 'package:widgets_app_4/presentation/screens/ui_controls/ui_controls_screen.dart';
