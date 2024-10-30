056e6a0b8e083f7d605fb78649f413ea3229bb50
Adrian Garcia <adrianisa123575@outlook.com>
2024-10-30 05:10:27 -0500

    Progress indicator terminado

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -28,5 +28,11 @@ const appMenuItems = <MenuItem> [
     subTitle: 'Un contenedor estilizado',
     link: '/cards',
     icon: Icons.credit_card
+  ),
+  MenuItem(
+    title: 'ProgressIndicators',
+    subTitle: 'Generales y controladores',
+    link: '/progress',
+    icon: Icons.refresh_rounded
   )
 ];
--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -21,6 +21,13 @@ final appRouter = GoRouter(
       path: '/cards',
       name: CardsScreen.name,
       builder: (context, state) => const CardsScreen(),
+    ),
+
+    GoRoute(
+      path: '/progress',
+      name: ProgressScreen.name,
+      builder: (context, state) => const ProgressScreen()
     )
+    
   ]
 );
